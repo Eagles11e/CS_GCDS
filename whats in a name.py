b4dcb8bc22ee34c0db9464ea26211938c7f65015
Eagles11e <Elisker28@gcds.net>
2026-09-18 08:41:45 -0400

    Add files via upload

---
+++ whats in a name.py
@@ -0,0 +1,56 @@
+"""
+
+
+"""
+
+
+def mylower(text):
+    result = []
+    for char in text:
+        if 'A' <= char <= 'Z':
+            result.append(chr(ord(char) + 32))
+        else:
+            result.append(char)
+    return ''.join(result)  
+
+
+
+
+
+
+userinfo = input("Enter name/word(s) you have chosen? ")
+
+print(userinfo) 
+
+def menu():
+    userinput = input("""\n Pick which function to run:
+                      
+                      1) Reverse and Display
+                      2) Vowel Amount
+                      3) Consonant frequency
+                      4) Return First Name
+                      5) Return Last Name
+                      6) Return Middle Name(s)
+                      7) Return boolean if last name contains a hyphen
+                      8) Function to convert to lowercase
+                      
+                      
+                    
+                      """)   
+
+def revanddis():
+    base = userinfo
+    base.split
+        
+
+
+
+
+def vowel_counter(userinfo):
+    vowelcounter = 0
+    x = userinfo.mylower()
+    characters = "a", "e", "i", "o", "u"
+    for char in x:
+        vowelcounter += 1
+    else: print(f"There are {vowelcounter} vowels in your name/word(s)")
+    
