4c107285091709964c0a093bcbb3ebcb77eee9c1
Eagles11e <Elisker28@gcds.net>
2026-09-23 13:11:21 -0400

    Add files via upload

--- whats in a name.py
+++ whats in a name.py
@@ -1,28 +1,35 @@
 """
+Ethan Lisker
 
-
+Funtions to manipulate and run things through strings
 """
 
 
 def mylower(text):
-    result = []
+    result = ""
     for char in text:
         if 'A' <= char <= 'Z':
-            result.append(chr(ord(char) + 32))
+            result = result + chr(ord(char) + 32)
         else:
-            result.append(char)
-    return ''.join(result)  
+            result = result + char 
+    return result
 
-
+def myjoin(item_list, separator):
+    joined_string = ""
+    
+    for i in range(len(item_list)):
+        joined_string = joined_string + str(item_list[i])
+        
+        if i < len(item_list) - 1:
+            joined_string = joined_string + separator    
 
 
 
 
 userinfo = input("Enter name/word(s) you have chosen? ")
 
-print(userinfo) 
 
-def menu():
+def main(userinfo):
     userinput = input("""\n Pick which function to run:
                       
                       1) Reverse and Display
@@ -36,21 +43,80 @@ def menu():
                       
                       
                     
-                      """)   
+                      """)
+    
+    if userinput == ('1'):
+        revanddis(userinfo)
+    if userinput == ('2'):
+        vowel_counter(userinfo)
+    if userinput == ('3'):
+        consonant_frequency(userinfo)
+    if userinput == ('4'):
+        first_name(userinfo)
+    if userinput == ('5'):
+        last_name(userinfo)
+    if userinput == ('7'):
+        has_hyphen(userinfo)
+    else:
+        print("Please give a number as outlined in the menu.")
+        main(userinfo)
 
-def revanddis():
-    base = userinfo
-    base.split
-        
-
-
-
+def revanddis(string):
+    revanddis = ""
+    for char in string:
+        revanddis = char + revanddis
+    print(revanddis)
 
 def vowel_counter(userinfo):
+    userinfo = mylower(userinfo)
     vowelcounter = 0
-    x = userinfo.mylower()
-    characters = "a", "e", "i", "o", "u"
+    x = userinfo
+    characters = ("a", "e", "i", "o", "u")
     for char in x:
-        vowelcounter += 1
-    else: print(f"There are {vowelcounter} vowels in your name/word(s)")
+        if char in characters:
+            vowelcounter += 1
+    print(f"There are {vowelcounter} vowels in your name/word(s)")
+
+def consonant_frequency(userinfo):
+    userinfo = mylower(userinfo)
+    count = 0
+    x = userinfo
+    characters = ("a", "e", "i", "o", "u")
+    for char in userinfo:
+        if char not in characters:
+            count += 1
+    print (f"There are {count} consonants in your name/wordS(s)")
+
+def first_name(userinfo):
+    first = []
+    for char in userinfo:
+        if char == " ":
+            break
+        first.append(char)
+    
+    first_name_str = myjoin("",first)
+    print(f"Your first name is: {first_name_str}")
+
+def last_name(userinfo):
+    last = []
+    for char in userinfo:
+        if char == " ":
+            break
+        last.append(char)
+    
+    last_name_str = myjoin("",last)
+    print(f"Your last name is: {last_name_str}")
+    
+
+def has_hyphen(userinfo):
+    found_hyphen = False
+    for char in userinfo:
+        if char == '-':
+            found_hyphen = True
+            break
+    print(f"Last name contains a hyphen: {found_hyphen}")
+
+
+
+main(userinfo)
     
